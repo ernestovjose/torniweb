16b3b62766dc70e1fb839b11dd2c10d7b3723421
ernestovjose <ernestovjose@gmail.com>
2020-09-01 18:17:53 -0400

    productos se mustran desde firebase

---
+++ src/app/services/firestore/firestore.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+import { Product } from '../../models/product'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirestoreService {
+
+  productsCollections;
+  products: Observable<Product[]>;
+  productsDoc;
+
+  constructor(public db: AngularFirestore) {
+    this.products = this.db.collection('products').valueChanges();
+  }
+
+  getProducts() {
+    return this.products;
+  }
+}
